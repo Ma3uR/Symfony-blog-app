3306456fec436eb08096a1ed556d2bffd88e7b41
Ma3uR <AndreyMazur99@gmail.com>
2020-10-19 11:23:11 +0300

    added Hello world Controller

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class HelloController extends AbstractController
+{
+    /**
+     * @Route("/hello", name="app_hello")
+     */
+    public function hello(): Response
+    {
+        return new Response(
+            $this->render('hello.html.twig')
+        );
+    }
+}
